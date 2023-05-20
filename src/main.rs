4bcda4cf9bb173d44c455f009ac83aa2b74c644f
Ahmad Anwar <eng.ahmed.anwar@gmail.com>
2023-05-20 15:41:18 +1200

    create board

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+#[derive(Debug, Copy, Clone)]
+enum Cell {
+    Empty,
+    X,
+    O,
+}
+
+#[derive(Debug)]
+struct Board {
+    cells: [Cell; 9],
+}
+
+impl Board {
+    fn new() -> Board {
+        Board {
+            cells: [Cell::Empty; 9],
+        }
+    }
+
+    fn display(&self) {
+        for (i, cell) in self.cells.iter().enumerate() {
+            match cell {
+                Cell::Empty => print!("   "),
+                Cell::X => print!(" X "),
+                Cell::O => print!(" O "),
+            }
+
+            if i % 3 == 2 {
+                println!();
+                if i != 8 {
+                    println!("-----------");
+                }
+            } else {
+                print!("|");
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let board = Board::new();
+    board.display();
 }
